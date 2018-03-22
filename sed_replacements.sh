0c3ab7649282909daf109b81b1a31bd713fa1251
danny316p <316danny@gmail.com>
2018-03-21 20:25:02 -0400

    Begin a sed script to edit the front page

---
+++ sed_replacements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# A quick script for sed find/replace calls
+# Run it from the root of the repository
+
+sed -i 's["BZPower%20-%20Bionicle%E2%84%A2%20and%20LEGO%C2%AE%20News,%20Reference%20and%20Discussion_files/bg_right.gif"[032018-graphics/bg_right.png[g' BZPower\ -\ Bionicle™\ and\ LEGO®\ News\,\ Reference\ and\ Discussion.html
+
