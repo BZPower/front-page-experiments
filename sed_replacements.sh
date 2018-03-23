ed9e28f6977c6bbbb8fa6467b9e2ae69bcd828c8
danny316p <316danny@gmail.com>
2018-03-22 21:32:31 -0400

    kill bg_topleft.gif

--- sed_replacements.sh
+++ sed_replacements.sh
@@ -3,6 +3,7 @@
 # A quick script for sed find/replace calls
 # Run it from the root of the repository
 
+# swap out image assets
 sed -i 's["BZPower%20-%20Bionicle%E2%84%A2%20and%20LEGO%C2%AE%20News,%20Reference%20and%20Discussion_files/bg.gif"[032018-graphics/bg.png[g' BZPower\ -\ Bionicle™\ and\ LEGO®\ News\,\ Reference\ and\ Discussion.html
 sed -i 's["BZPower%20-%20Bionicle%E2%84%A2%20and%20LEGO%C2%AE%20News,%20Reference%20and%20Discussion_files/bg_left.gif"[032018-graphics/bg_left.png[g' BZPower\ -\ Bionicle™\ and\ LEGO®\ News\,\ Reference\ and\ Discussion.html
 sed -i 's["BZPower%20-%20Bionicle%E2%84%A2%20and%20LEGO%C2%AE%20News,%20Reference%20and%20Discussion_files/bg_right.gif"[032018-graphics/bg_right.png[g' BZPower\ -\ Bionicle™\ and\ LEGO®\ News\,\ Reference\ and\ Discussion.html
@@ -28,6 +29,22 @@ sed -i 's["BZPower%20-%20Bionicle%E2%84%A2%20and%20LEGO%C2%AE%20News,%20Referenc
 # 032018-graphics/mast_topright_vahi.png
 # 032018-graphics/mast_topright_vezok.png
 # 032018-graphics/mast_topright_voya_nui.png
+
+
+
+
+# Replace an unneeded image with a generic one
+sed -i 's["BZPower%20-%20Bionicle%E2%84%A2%20and%20LEGO%C2%AE%20News,%20Reference%20and%20Discussion_files/bg_topleft.gif"[032018-graphics/bg_left.png[g' BZPower\ -\ Bionicle™\ and\ LEGO®\ News\,\ Reference\ and\ Discussion.html
+
+
+
+
+
+
+
+
+
+
 
 
 
